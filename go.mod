940dfcce92540c2e7d5f9c0ac72570c407bd77ab
Jay Ehsaniara <ehsaniara@gmail.com>
2025-07-07 08:06:08 -0700

    add dynamic sizing for list cmd

--- go.mod
+++ go.mod
@@ -4,21 +4,20 @@ go 1.24
 
 require (
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/maxbrunsfeld/counterfeiter/v6 v6.11.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- internal/rnx/list.go
+++ internal/rnx/list.go
@@ -3,6 +3,7 @@ package rnx
 import (
 	"context"
 	"fmt"
+	pb "joblet/api/gen"
 	"strings"
 	"time"
 
@@ -20,7 +21,7 @@ func newListCmd() *cobra.Command {
 }
 
 func runList(cmd *cobra.Command, args []string) error {
-	// SIMPLIFIED: One line client creation using unified config
+
 	jobClient, err := newJobClient()
 	if err != nil {
 		return fmt.Errorf("failed to create client: %w", err)
@@ -40,10 +41,85 @@ func runList(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	for _, job := range response.Jobs {
-		fmt.Printf("%s %s StartTime: %s Command: %s %s\n",
-			job.Id, job.Status, job.StartTime, job.Command, strings.Join(job.Args, " "))
-	}
+	formatJobList(response.Jobs)
 
 	return nil
+}
+
+func formatJobList(jobs []*pb.Job) {
+	maxIDWidth := len("ID")
+	maxStatusWidth := len("STATUS")
+
+	// find the maximum width needed for each column
+	for _, job := range jobs {
+		if len(job.Id) > maxIDWidth {
+			maxIDWidth = len(job.Id)
+		}
+		if len(job.Status) > maxStatusWidth {
+			maxStatusWidth = len(job.Status)
+		}
+	}
+
+	// add some padding
+	maxIDWidth += 2
+	maxStatusWidth += 2
+
+	// header
+	fmt.Printf("%-*s %-*s %-19s %s\n",
+		maxIDWidth, "ID",
+		maxStatusWidth, "STATUS",
+		"START TIME",
+		"COMMAND")
+
+	// separator line
+	fmt.Printf("%s %s %s %s\n",
+		strings.Repeat("-", maxIDWidth),
+		strings.Repeat("-", maxStatusWidth),
+		strings.Repeat("-", 19), // length of "START TIME"
+		strings.Repeat("-", 7))  // length of "COMMAND"
+
+	// each job
+	for _, job := range jobs {
+
+		startTime := formatStartTime(job.StartTime)
+
+		// truncate long commands
+		command := formatCommand(job.Command, job.Args)
+
+		fmt.Printf("%-*s %-*s %-19s %s\n",
+			maxIDWidth, job.Id,
+			maxStatusWidth, job.Status,
+			startTime,
+			command)
+	}
+}
+
+func formatStartTime(timeStr string) string {
+	if timeStr == "" {
+		return "N/A"
+	}
+
+	// Parse the RFC3339 timestamp
+	t, err := time.Parse(time.RFC3339, timeStr)
+	if err != nil {
+		return timeStr
+	}
+
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func formatCommand(command string, args []string) string {
+	if len(args) == 0 {
+		return command
+	}
+
+	fullCommand := command + " " + strings.Join(args, " ")
+
+	// truncate very long commands
+	maxCommandLength := 80
+	if len(fullCommand) > maxCommandLength {
+		return fullCommand[:maxCommandLength-3] + "..."
+	}
+
+	return fullCommand
 }
